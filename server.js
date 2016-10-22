795dfe705710f32e84a339e8c1e46a06583a538b
JO <jondrus@gmail.com>
2016-10-22 11:54:18 -0500

    Add bootstrap.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.use(express.static(__dirname + '/'));
+
 
 app.use('/modules', express.static(__dirname + '/node_modules/'));
 app.use('/styles', express.static(__dirname + '/styles/'));
@@ -11,9 +13,12 @@ app.use('/views', express.static(__dirname + '/views/'));
 app.use('/images', express.static(__dirname + '/images/'));
 app.use('/', express.static(__dirname + '/'));
 
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 app.listen(8080);
+
