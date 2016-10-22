0886d2aaaa63a2976d39f7ddccdf5daf226c5098
JO <jondrus@gmail.com>
2016-10-22 14:29:38 -0500

    All pages now go somewhere

--- src/app.js
+++ src/app.js
@@ -13,16 +13,17 @@ var states = [
         { name: 'login', state: { url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl', data: {text: "Login", visible: false } } },
         { name: 'dashboard', state: { url: '/dashboard', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'DashboardCtrl', data: {text: "Dashboard", visible: false } } },
 
-        { name: 'cmgr', state: { url: '/cmgr', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'CaseManagerCtrl', data: {text: "Case Manager0", visible: false } } },
-        { name: 'svcpro', state: { url: '/svcpro', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'ServiceProviderCtrl', data: {text: "Case Manager1", visible: false } } },
+        { name: 'cmgr', state: { url: '/cmgr', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'CaseManagerCtrl', data: {text: "Dashboard", visible: false } } },
+        { name: 'svcpro', state: { url: '/svcpro', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'ServiceProviderCtrl', data: {text: "Dashboard", visible: false } } },
+        { name: 'client', state: { url: '/client', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'ClientCtrl', data: {text: "Dashboard", visible: false } } },
 
         { name: 'overview', state: { url: '/overview', parent: 'dashboard', templateUrl: 'views/dashboard/overview.html', data: {text: "Overview", visible: true } } },
         { name: 'reports', state: { url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html', data: {text: "Reports", visible: true } } },
 
 
-        { name: 'cmgrlanding', state: { url: '/cmgrlanding', parent: 'cmgr', templateUrl: 'views/landings/casemanager.html', data: {text: "Case Manager2", visible: true } } },
-
-        { name: 'svcprolanding', state: { url: '/svcprolanding', parent: 'svcpro', templateUrl: 'views/landings/serviceprovider.html', data: {text: "Case Manager3", visible: true } } },
+        { name: 'cmgrlanding', state: { url: '/cmgrlanding', parent: 'cmgr', templateUrl: 'views/landings/casemanager.html', data: {text: "Home", visible: true } } },
+        { name: 'svcprolanding', state: { url: '/svcprolanding', parent: 'svcpro', templateUrl: 'views/landings/serviceprovider.html', data: {text: "Home", visible: true } } },
+        { name: 'clientlanding', state: { url: '/clientlanding', parent: 'client', templateUrl: 'views/landings/client.html', data: {text: "Home", visible: true } } },
 
         { name: 'logout', state: { url: '/login', data: {text: "Logout", visible: true }} }
     ];
@@ -32,6 +33,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
     $urlRouterProvider.when('/cmgr', '/cmgr/cmgrlanding');
     $urlRouterProvider.when('/svcpro', '/svcpro/svcprolanding');
+    $urlRouterProvider.when('/client', '/client/clientlanding');
     $urlRouterProvider.otherwise('/dashboard');
 
 
