5f6becbc6580066417b10083542800404c90f809
JO <jondrus@gmail.com>
2016-10-22 14:09:23 -0500

    Fixed css issues and routing.

--- src/app.js
+++ src/app.js
@@ -13,16 +13,16 @@ var states = [
         { name: 'login', state: { url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl', data: {text: "Login", visible: false } } },
         { name: 'dashboard', state: { url: '/dashboard', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'DashboardCtrl', data: {text: "Dashboard", visible: false } } },
 
-        { name: 'cmgr', state: { url: '/cmgr', parent: 'base', templateUrl: 'views/landings/casemanager.html', controller: 'CaseManagerCtrl', data: {text: "Case Manager", visible: false } } },
-        { name: 'svcpro', state: { url: '/svcpro', parent: 'base', templateUrl: 'views/landings/serviceprovider.html', controller: 'CaseManagerCtrl', data: {text: "Case Manager", visible: false } } },
+        { name: 'cmgr', state: { url: '/cmgr', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'CaseManagerCtrl', data: {text: "Case Manager0", visible: false } } },
+        { name: 'svcpro', state: { url: '/svcpro', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'ServiceProviderCtrl', data: {text: "Case Manager1", visible: false } } },
 
         { name: 'overview', state: { url: '/overview', parent: 'dashboard', templateUrl: 'views/dashboard/overview.html', data: {text: "Overview", visible: true } } },
         { name: 'reports', state: { url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html', data: {text: "Reports", visible: true } } },
 
 
-        { name: 'cmgrlanding', state: { url: '/landing', parent: 'cmgr', templateUrl: 'views/landings/casemanager.html', data: {text: "Case Manager1", visible: true } } },
+        { name: 'cmgrlanding', state: { url: '/cmgrlanding', parent: 'cmgr', templateUrl: 'views/landings/casemanager.html', data: {text: "Case Manager2", visible: true } } },
 
-        { name: 'svcprolanding', state: { url: '/landing', parent: 'svcpro', templateUrl: 'views/landings/serviceprovider.html', data: {text: "Case Manager1", visible: true } } },
+        { name: 'svcprolanding', state: { url: '/svcprolanding', parent: 'svcpro', templateUrl: 'views/landings/serviceprovider.html', data: {text: "Case Manager3", visible: true } } },
 
         { name: 'logout', state: { url: '/login', data: {text: "Logout", visible: true }} }
     ];
@@ -30,9 +30,9 @@ var states = [
 var app = angular.module('yapp', ['ui.router','snap', 'ngAnimate']);
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
-    $urlRouterProvider.when('/cmgr', '/cmgr/landing');
-    $urlRouterProvider.when('/svcpro', '/svcpro/landing');
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.when('/cmgr', '/cmgr/cmgrlanding');
+    $urlRouterProvider.when('/svcpro', '/svcpro/svcprolanding');
+    $urlRouterProvider.otherwise('/dashboard');
 
 
     angular.forEach(states, function (state) {
--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -30,3 +30,14 @@ app.controller('CaseManagerCtrl', function($scope, $state) {
         }
     });
 });
+
+app.controller('ServiceProviderCtrl', function($scope, $state) {
+    $scope.$state = $state;
+
+    $scope.menuItems = [];
+    angular.forEach($state.get(), function (item) {
+        if (item.data && item.data.visible) {
+            $scope.menuItems.push({name: item.name, text: item.data.text});
+        }
+    });
+});
