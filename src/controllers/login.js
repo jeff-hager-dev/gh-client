2237d27e888b84a7b6ae2279a45cb56c8a4dfd2b
Adam Frank <adam.frank@daugherty.com>
2016-10-22 23:32:59 -0500

    client profile page works

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -8,43 +8,44 @@
  * Controller of yapp
  */
 var login = angular.module('yapp');
-login.controller('LoginCtrl', function($scope, $location) {
-
-        $scope.submit = function() {
-
+login.controller('LoginCtrl', function ($scope, $location) {
+    
+    $scope.submit = function () {
+        
         $location.path('/dashboard');
-
+        
         return false;
     }
-
+    
 });
-login.controller('ClientLoginCtrl', function($scope, $location) {
-
-    $scope.submit = function() {
-
+login.controller('ClientLoginCtrl', function ($scope, $location, $rootScope) {
+    
+    $scope.submit = function () {
+        $rootScope.username = $scope.username
+        
         $location.path('/client/profile');
-
+        
         return false;
     }
-
+    
 });
-login.controller('CaseManagerLoginCtrl', function($scope, $location) {
-
-    $scope.submit = function() {
-
+login.controller('CaseManagerLoginCtrl', function ($scope, $location) {
+    
+    $scope.submit = function () {
+        
         $location.path('/cmgr/cmgrlanding');
-
+        
         return false;
     }
-
+    
 });
-login.controller('ServiceProviderLoginCtrl', function($scope, $location) {
-
-    $scope.submit = function() {
-
+login.controller('ServiceProviderLoginCtrl', function ($scope, $location) {
+    
+    $scope.submit = function () {
+        
         $location.path('/svcpro/svcprolanding');
-
+        
         return false;
     }
-
+    
 });
