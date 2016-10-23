7cd8aa04f589d2a0dba07046298238033fe5d41d
jeff-hager-dev <jeffrey.hager.dev@gmail.com>
2016-10-22 20:55:39 -0500

    Working on the client profile

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -3,6 +3,10 @@ profile.controller('ClientProfileCtrl', function($scope) {
   $scope.ClientModel = {
     "firstName" : "Matt",
     "lastName" : "Cronin",
-    "ssn" : "12456644"
+    "ssn" : "*****6644",
+    "contact": {
+      "phone": 1231231234,
+      "address": "4242 St. Patti's Center"
+    }
   };
 });
